d48a9920c0c31c02fdeba227511fc78235cfd943
dl10yr <tkzw04202166@gmail.com>
2021-06-10 22:26:32 +0900

    210610

---
+++ next.config.js
@@ -0,0 +1,3 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require('next-transpile-modules')(['react-children-utilities'])
+module.exports = withTM()
---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,49 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import { getPostBySlug, getAllPosts } from '@/utils/utils'
+import Markdown from '@/components/Markdown'
+
+export default function Post({ post }) {
+  const router = useRouter()
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <div>
+      <Markdown source={post.content} />
+    </div>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const post = getPostBySlug(params.slug, [
+    'title',
+    'date',
+    'slug',
+    'author',
+    'content',
+    'ogImage',
+    'coverImage',
+  ])
+
+  return {
+    props: {
+      post,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(['slug'])
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
