a6cd640a6224175ee25ecc301adb62598f3a9af0
dl10yr <tkzw04202166@gmail.com>
2021-06-13 10:48:27 +0900

    fix

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -5,7 +5,7 @@ export const WorkCard: React.FC = () => {
     <a className="m-3 px-4 py-2 shadow hover:bg-gray-600 rounded-lg w-32">
       <div className="mr-2">画像</div>
       <div className="">
-        <div className="font-semibold">タイトルタイトル退路つ</div>
+        <div className="font-semibold">タイトルタイル退路つ</div>
         <div className="font-light">2020/12/12 zenn</div>
       </div>
     </a>
