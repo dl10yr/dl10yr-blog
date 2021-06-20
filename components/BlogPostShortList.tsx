532a0eafaa432a2f76a7f7b5f08478292775b7da
dl10yr <tkzw04202166@gmail.com>
2021-06-20 20:01:16 +0900

    210620

--- components/BlogPostShortList.tsx
+++ components/BlogPostShortList.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import { BlogPostLink } from '@/components/BlogPostLink'
 
-export const BlogPostShortList: React.FC = () => {
-  return <div className="border-t border-gray-500 py-1 my-1"></div>
+export interface BlogPostListInterface {
+  postsList: any
 }
+const BlogPostShortList: React.FC<BlogPostListInterface> = ({ postsList }) => {
+  return (
+    <div className="border-t border-gray-500 py-1 my-1">
+      {postsList.map((post) => {
+        return <BlogPostLink key={post.slug} date={post.date} title={post.title} slug={post.slug} />
+      })}
+    </div>
+  )
+}
+
+export default BlogPostShortList
