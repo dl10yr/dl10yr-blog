7349a7f59d0d531c8dffd0a441a020f7057a76d4
dl10yr <tkzw04202166@gmail.com>
2023-07-29 14:24:43 +0900

    fix linkcard

--- components/markdown/LinkCard.tsx
+++ components/markdown/LinkCard.tsx
@@ -10,22 +10,28 @@ interface LinkCardProps {
 
 const LinkCard: React.FC<LinkCardProps> = ({ href, metas }) => {
   const target = metas.find((meta) => meta.url === href)
-
+  if (target?.description || target?.image || target?.title) {
+    return (
+      <a href={href} target="_blank" rel="noreferrer">
+        <div className="w-full flex justify-around bg-gray-200 rounded-md m-3 p-3 border lg:w-1/2">
+          <div className="w-1/2">
+            <img src={target?.image || ''} alt={target?.title || ''} className="max-h-20 m-auto" />
+          </div>
+          <div className="flex flex-col justify-start px-1 ml-3">
+            <div className="text-sm font-bold text-black whitespace-pre-wrap">
+              {target?.title || ''}
+            </div>
+            <div className="text-gray-400 text-xs whitespace-pre-wrap">
+              {target?.description || ''}
+            </div>
+          </div>
+        </div>
+      </a>
+    )
+  }
   return (
     <a href={href} target="_blank" rel="noreferrer">
-      <div className="w-full flex justify-around bg-white rounded-md p-3 border lg:w-1/2">
-        <div className="w-1/2">
-          <img src={target?.image || ''} alt={target?.title || ''} className="max-h-20 m-auto" />
-        </div>
-        <div className="flex flex-col justify-start px-1 ml-3">
-          <div className="text-sm font-bold text-black whitespace-pre-wrap">
-            {target?.title || ''}
-          </div>
-          <div className="text-gray-400 text-xs whitespace-pre-wrap">
-            {target?.description || ''}
-          </div>
-        </div>
-      </div>
+      {href}
     </a>
   )
 }
