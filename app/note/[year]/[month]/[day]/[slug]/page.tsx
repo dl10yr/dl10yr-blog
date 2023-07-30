e4caeea85d534755b63078c69b293a6581955aaa
dl10yr <tkzw04202166@gmail.com>
2023-07-30 12:07:15 +0900

    add classname of h-entry

--- app/note/[year]/[month]/[day]/[slug]/page.tsx
+++ app/note/[year]/[month]/[day]/[slug]/page.tsx
@@ -15,12 +15,17 @@ export default async function Page({ params }) {
 
   return (
     <div className="w-full">
-      <article className="p-5 lg:rounded-lg">
+      <article className="p-5 lg:rounded-lg h-entry">
         <div className="info p-2">
           <div className="py-2">{note.date}</div>
         </div>
         <div>
-          <div>{note.content}</div>
+          <div className="e-content">{note.content}</div>
+        </div>
+        <div className="hidden">
+          <div className="p-name">{note.content}</div>
+          <a className="u-bridgy-fed" href="https://fed.brid.gy"></a>
+          <p className="dt-published">{note.date}</p>
         </div>
       </article>
     </div>
