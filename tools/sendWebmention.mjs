b5fb1c00a7e5adb0f5eafde04cadb09ee82c733a
dl10yr <tkzw04202166@gmail.com>
2023-07-31 17:40:41 +0900

    fix workflow

--- tools/sendWebmention.mjs
+++ tools/sendWebmention.mjs
@@ -2,25 +2,34 @@ const child_process = await import('child_process')
 
 const MAX_MENTION_SENDS = 10
 
-const args = process.argv.slice(2)
+const sleep = (sec) => new Promise((res) => setTimeout(res, sec * 1000))
 
-const diff = child_process
-  .spawnSync('git', ['diff', '--name-only', args[0], args[1]])
-  .stdout.toString()
-const files = diff.split('\n')
+async function sendWebmention(oldCommitHash, newCommitHash) {
+  const diff = child_process
+    .spawnSync('git', ['diff', '--name-only', oldCommitHash, newCommitHash])
+    .stdout.toString()
+  const files = diff.split('\n')
 
-const filtered = files.filter((file) => file.indexOf('_notes/') !== -1)
+  const filtered = files.filter((file) => file.indexOf('_notes/') !== -1)
 
-for (let i = 0; i < filtered.length && i < MAX_MENTION_SENDS; ++i) {
-  const filePath = filtered[i].split('/')
-  const linkPath = filePath.slice(1, -1)
-  const url = 'https://dl10yr.com/note/' + linkPath.join('/')
-  console.log('sending webmention of ' + url)
-  child_process.spawnSync('curl', [
-    'https://fed.brid.gy/webmention',
-    '-d',
-    `source=${encodeURIComponent(url)}`,
-    '-d',
-    `target=https://fed.brid.gy`,
-  ])
+  if (filtered.length >= 1) {
+    await sleep(110)
+  }
+
+  for (let i = 0; i < filtered.length && i < MAX_MENTION_SENDS; ++i) {
+    const filePath = filtered[i].split('/')
+    const linkPath = filePath.slice(1, -1)
+    const url = 'https://dl10yr.com/note/' + linkPath.join('/')
+    console.log('sending webmention of ' + url)
+    child_process.spawnSync('curl', [
+      'https://fed.brid.gy/webmention',
+      '-d',
+      `source=${encodeURIComponent(url)}`,
+      '-d',
+      `target=https://fed.brid.gy`,
+    ])
+  }
 }
+
+const args = process.argv.slice(2)
+sendWebmention(args[0], args[1])
