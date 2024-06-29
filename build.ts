7e0196f4dfcf99723cd803c73b31f723c8863c51
dl10yr <tkzw04202166@gmail.com>
2024-06-29 22:18:17 +0900

    add build.ts

---
+++ build.ts
@@ -0,0 +1,14 @@
+import { toSSG } from "hono/bun";
+// import { generateSitemap } from "sitemap-ts";
+import app from "./src/index";
+
+toSSG(app);
+
+const hostname = process.env.VITE_GIALOG_BASE_URL;
+const basePath = process.env.VITE_BASE_PATH;
+// generateSitemap({
+//   outDir: "./static",
+//   readable: true,
+//   hostname,
+//   basePath,
+// });
